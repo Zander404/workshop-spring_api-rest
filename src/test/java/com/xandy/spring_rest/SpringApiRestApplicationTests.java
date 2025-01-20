cd4c6cb0c709805560d07a9d2bd51366ffbe955e
xandy <>
2025-01-20 13:59:27 -0400

    /init: start project

---
+++ src/test/java/com/xandy/spring_rest/SpringApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xandy.spring_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
