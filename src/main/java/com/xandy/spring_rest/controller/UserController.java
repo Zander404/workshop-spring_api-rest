70d10dbce8812e1cf8400e3c02ffc99e2fa3f8d0
xandy <>
2025-01-20 20:44:59 -0400

    /feature: add endpoint to User Controlller

--- src/main/java/com/xandy/spring_rest/controller/UserController.java
+++ src/main/java/com/xandy/spring_rest/controller/UserController.java
@@ -6,10 +6,9 @@ import com.xandy.spring_rest.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,9 +17,28 @@ public class UserController {
 
     private final UserService services;
 
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = services.getAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = services.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
     @PostMapping
     public ResponseEntity<User> create(@RequestBody User obj) {
         User user = services.save(obj);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updatePassword(@PathVariable Long id, @RequestBody String password) {
+        User user = services.updatePassword(id, password);
+        return ResponseEntity.ok().body(user);
+    }
+
 }
--- src/main/java/com/xandy/spring_rest/services/UserService.java
+++ src/main/java/com/xandy/spring_rest/services/UserService.java
@@ -2,16 +2,34 @@ package com.xandy.spring_rest.services;
 
 import com.xandy.spring_rest.entities.User;
 import com.xandy.spring_rest.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository repository;
 
+
     public User save(User user) {
-        System.out.println(user);
+
+        return repository.save(user);
+    }
+
+    public User findById(Long id) {
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("User not Find"));
+    }
+
+    public List<User> getAll() {
+        return repository.findAll();
+    }
+
+    public User updatePassword(Long id, String newPassword) {
+        User user = findById(id);
+        user.setPassword(newPassword);
         return repository.save(user);
     }
 }
