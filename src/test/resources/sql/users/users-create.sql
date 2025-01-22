69ab203a0a8daafe61bc8672a290ddf3055eb89b
xandy <>
2025-01-22 14:23:01 -0400

    /feature: create Unit Tests to User

---
+++ src/test/resources/sql/users/users-create.sql
@@ -0,0 +1,3 @@
+INSERT INTO USERS (id, username, password, role) VALUES (100, "ana1@gmail.com", "123456", "ROLE_ADMIN");
+INSERT INTO USERS (id, username, password, role) VALUES (101, "bia@gmail.com", "123456", "ROLE_ADMIN");
+INSERT INTO USERS (id, username, password, role) VALUES (102, "bob@gmail.com", "123456", "ROLE_ADMIN");
