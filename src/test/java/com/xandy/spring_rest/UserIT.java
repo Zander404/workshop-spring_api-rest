592411c66b83421b160c280cf6a6d64d84df0c00
xandy <>
2025-01-22 14:51:54 -0400

    /feature: create Unit Tests to User

--- src/test/java/com/xandy/spring_rest/UserIT.java
+++ src/test/java/com/xandy/spring_rest/UserIT.java
@@ -1,7 +1,11 @@
 package com.xandy.spring_rest;
 
+import com.xandy.spring_rest.dto.UserCreateDTO;
+import com.xandy.spring_rest.dto.UserResponseDTO;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -12,4 +16,26 @@ public class UserIT {
 
     @Autowired
     WebTestClient testClient;
+
+    @Test
+    public void createUserValid() {
+
+        UserResponseDTO responseBody = testClient
+                .post()
+                .uri("/api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateDTO("xandy@gmail.com", "123456"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(UserResponseDTO.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getUsername()).isEqualTo("xandy@gmail.com");
+        org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("USER");
+
+
+    }
+
 }
